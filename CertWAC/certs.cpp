2559cb4c193a6392c1cb2978868114948278319a
Eric Siron <ejsiron@users.noreply.github.com>
2019-02-15 15:53:32 -0600

    2/15/19 midday

--- CertWAC/certs.cpp
+++ CertWAC/certs.cpp
@@ -1,17 +1,18 @@
 #include <Windows.h>
-#include <Wincrypt.h>
-#include <array>
+#include <wincrypt.h>
+#include <iomanip>
 #include <sstream>
 #include "certs.h"
 #pragma comment(lib, "crypt32.lib")
 
 void GetComputerCertificates()
 {
-	HANDLE hStoreHandle{ 0 };
 	PCCERT_CONTEXT pCertContext{ 0 };
 	const wchar_t* StoreName{ L"MY" };
+	HCERTSTORE CertificateStore{ NULL };
 
-	if (hStoreHandle = CertOpenSystemStore(NULL, StoreName))
+	if (CertificateStore = CertOpenStore(CERT_STORE_PROV_SYSTEM_W, 0, NULL,
+		CERT_SYSTEM_STORE_LOCAL_MACHINE, StoreName))
 	{
 		// this is good
 	}
@@ -19,14 +20,15 @@ void GetComputerCertificates()
 	{
 		// this is not, GetLastError has deets
 	}
-	
+
 	DWORD PropID{ 0 };
-	while (pCertContext = CertEnumCertificatesInStore(hStoreHandle, pCertContext))
+	while (pCertContext = CertEnumCertificatesInStore(CertificateStore, pCertContext))
 	{
 		DWORD CertHashSize{ 0 };
 		if (CryptHashCertificate2(BCRYPT_SHA1_ALGORITHM, 0, NULL,
 			pCertContext->pbCertEncoded, pCertContext->cbCertEncoded, NULL, &CertHashSize))
 		{
+			// thumbprint
 			auto pCertHash{ new BYTE[CertHashSize] };
 			SecureZeroMemory(pCertHash, sizeof(BYTE) * CertHashSize);
 			// https://github.com/Microsoft/TSS.MSR/blob/master/PCPTool.v11/exe/SDKSample.cpp
@@ -36,16 +38,23 @@ void GetComputerCertificates()
 				std::stringstream CertHashStream;
 				CertHashStream << std::hex;
 				for (auto i{ 0 }; i != CertHashSize; ++i)
-					CertHashStream << pCertHash[i];
-				CertHashStream << L'\n';
+					CertHashStream << std::setw(2) << std::setfill('0') << static_cast<int>(pCertHash[i]);
+				CertHashStream << " END \r\n";
 				std::string CertHashString{ CertHashStream.str() };
 				OutputDebugStringA(CertHashString.c_str());
 			}
 			delete[] pCertHash;
+
+			
 		}
 	}
 
-	if (!CertCloseStore(hStoreHandle, 0))
+	if (!CertFreeCertificateContext(pCertContext))
+	{
+		OutputDebugString(L"Could not free certificate\n");
+	}
+
+	if (!CertCloseStore(CertificateStore, 0))
 	{
 		OutputDebugString(L"Could not close store\n");
 	}
--- CertWAC/certs.h
+++ CertWAC/certs.h
@@ -1,3 +1,12 @@
 #pragma once
+#include <Windows.h>
+#include <array>
 
+constexpr size_t SHA1_BYTE_LENGTH{ 20 };
 void GetComputerCertificates();
+class StoreCertificates
+{
+private:
+	std::array<BYTE, SHA1_BYTE_LENGTH> Thumbprint{0};
+
+};
