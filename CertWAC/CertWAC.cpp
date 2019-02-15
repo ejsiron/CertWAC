d33a83a91f183e61b0f0284abbf1d4928b0493ee
Eric Siron <ejsiron@users.noreply.github.com>
2019-02-14 20:43:59 -0600

    2/14/19 additions

--- CertWAC/CertWAC.cpp
+++ CertWAC/CertWAC.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <tchar.h>
+#include <utility>
 #include "resource.h"
 
 INT_PTR CALLBACK DialogProc(HWND hDialog, UINT uMessage, WPARAM wParam, LPARAM lParam)
@@ -24,8 +25,42 @@ INT_PTR CALLBACK DialogProc(HWND hDialog, UINT uMessage, WPARAM wParam, LPARAM l
 	return FALSE;
 }
 
+std::pair<bool, DWORD> IsAdmin()
+{
+	try
+	{
+		HANDLE hCurrentProcess{ GetCurrentProcess() };
+		HANDLE hProcessToken{ 0 };
+		if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hProcessToken))
+			throw;
+
+		TOKEN_ELEVATION_TYPE ElevationType;
+		DWORD TokenSize;
+		if (!GetTokenInformation(hProcessToken, TokenElevationType, &ElevationType, sizeof(ElevationType), &TokenSize))
+			throw;
+
+		bool IsAdmin{ ElevationType == TOKEN_ELEVATION_TYPE::TokenElevationTypeFull };
+		return std::make_pair(IsAdmin, ERROR_SUCCESS);
+
+		//TOKEN_ELEVATION Elevation;
+		//if (!GetTokenInformation(hProcessToken, TokenElevation, &Elevation, sizeof(Elevation), &TokenSize))
+		//	throw;
+	}
+	catch (...)
+	{
+		return std::make_pair(false, GetLastError());
+	}
+}
+
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
+	const auto[bIsAdmin, dwError] { IsAdmin()};
+	if (dwError != ERROR_SUCCESS)
+		OutputDebugStringW(L"Error!");
+	if (bIsAdmin)
+		OutputDebugStringW(L"Is admin");
+	else
+		OutputDebugStringW(L"Not admin");
 	HWND hMainDialog{ CreateDialogParam(hInstance, MAKEINTRESOURCE(IDD_MAIN), 0, DialogProc, 0) };
 	ShowWindow(hMainDialog, nCmdShow);
 	MSG msg;
